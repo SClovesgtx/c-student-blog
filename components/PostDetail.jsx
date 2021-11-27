cf917e728d37146756c2ae080a8e41b99c9f0423
SClovesgtx <clovestudent@gmail.com>
2021-11-27 18:03:54 -0300

    styles for contents

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -2,6 +2,60 @@ import React from 'react'
 import moment from 'moment';
 
 const PostDetail = ({ post }) => {
+    const getContentFragment = (index, text, obj, type) => {
+        let modifiedText = text;
+    
+        if (obj) {
+          if (obj.bold) {
+            modifiedText = (<b key={index}>{text}</b>);
+          }
+    
+          if (obj.italic) {
+            modifiedText = (<em key={index}>{text}</em>);
+          }
+    
+          if (obj.underline) {
+            modifiedText = (<u key={index}>{text}</u>);
+          }
+        }
+    
+        switch (type) {
+          case 'block-quote':
+            return <p 
+                className="p-4 italic border-l-4 bg-neutral-100 mb-8 text-neutral-600 border-neutral-500 quote"
+                key={index}
+                > 
+                    {modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)} 
+                </p>;
+          case 'code-block':
+              return <code 
+                className="block whitespace-pre overflow-x-scroll mb-8 bg-gray-800 text-white dark:text-gray-300"
+                key={index}
+                > 
+                    {modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)} 
+                </code>;
+          case 'heading-three':
+            return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+          case 'paragraph':
+            return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+          case 'heading-four':
+            return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+          case 'image':
+            return (
+              <img
+                key={index}
+                alt={obj.title}
+                height={obj.height}
+                width={obj.width}
+                src={obj.src}
+                className='mb-8'
+              />
+            );
+          default:
+            return modifiedText;
+        }
+    };
+
     return (
         <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
             <div className="relative overflow-hidden shadow-md mb-6">
@@ -38,6 +92,11 @@ const PostDetail = ({ post }) => {
                     </div>
                 </div>
                 <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+                {console.log("POST:", post)}
+                {post.content.raw.children.map((typeObj, index) => {
+                    const children = typeObj.children.map( (item, itemIndex) => getContentFragment(itemIndex, item.text, item))
+                    return getContentFragment(index, children, typeObj, typeObj.type);
+                })}
             </div>
         </div>
     )
