c5f7332b1d4a40c70c6806d8f054483206f32ac6
SClovesgtx <clovestudent@gmail.com>
2021-11-27 16:22:37 -0300

    post detail - header

--- services/index.js
+++ services/index.js
@@ -36,6 +36,40 @@ export const getPosts = async () => {
     return result.postsConnection.edges;
 }
 
+export const getPostDetails = async (slug) => {
+    const query = gql`
+      query GetPostDetails($slug : String!) {
+        post(where: {slug: $slug}) {
+          title
+          excerpt
+          featuredImage {
+            url
+          }
+          author{
+            name
+            bio
+            photo {
+              url
+            }
+          }
+          createdAt
+          slug
+          content {
+            raw
+          }
+          categories {
+            name
+            slug
+          }
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query, { slug });
+  
+    return result.post;
+};
+
 export const getRecentPosts = async () => {
     const query = gql`
         query GetPostDetails() {
@@ -57,7 +91,7 @@ export const getRecentPosts = async () => {
     return result.posts;
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
     const query = gql`
         query GetPostDetails($slug: String!, $categories: [String!]) {
             posts(
@@ -73,7 +107,7 @@ export const getSimilarPosts = async () => {
             }
         }
     `
-    const result = await request(graphqlAPI, query)
+    const result = await request(graphqlAPI, query, { categories, slug })
 
     return result.posts;
 }
@@ -91,3 +125,4 @@ export const getCategories = async () => {
 
     return result.categories;
 }
+
