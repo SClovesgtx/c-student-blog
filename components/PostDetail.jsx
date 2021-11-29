f68993c5bc4824210eadbb5324f303e8778bb776
SClovesgtx <clovestudent@gmail.com>
2021-11-29 07:20:25 -0300

    author infos

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -92,7 +92,6 @@ const PostDetail = ({ post }) => {
                     </div>
                 </div>
                 <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
-                {console.log("POST:", post)}
                 {post.content.raw.children.map((typeObj, index) => {
                     const children = typeObj.children.map( (item, itemIndex) => getContentFragment(itemIndex, item.text, item))
                     return getContentFragment(index, children, typeObj, typeObj.type);
