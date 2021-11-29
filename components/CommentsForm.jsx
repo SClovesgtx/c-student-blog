0f8ef683281066dbcd638486a39f9f3c304c226c
SClovesgtx <clovestudent@gmail.com>
2021-11-29 19:56:42 -0300

    comment forms

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -8,6 +8,28 @@ const CommentsForm = ({ slug }) => {
     const nameEl = useRef()
     const emailEl = useRef()
     const storeDataEl = useRef()
+
+    const handleCommentSubmission = () => {
+        setError(false);
+        const { value: comment } = commentEl.current;
+        const { value: name } = nameEl.current;
+        const { value: email } = emailEl.current;
+        const { checked: storeData } = storeDataEl.current;
+        if(!comment || !name || !email){
+            setError(true);
+            return;
+        }
+
+        const commentObj = {name, email, comment, slug};
+
+        if(storeData) {
+            localStorage.setItem('name', name);
+            localStorage.setItem('email', email);
+        } else {
+            localStorage.removeItem('name', name);
+            localStorage.removeItem('email', email);
+        }
+    }
     return (
         <div className="bg-white shodow-lg rounded-lg p-8 pb-12 mb-8">
             <h3 className="text-xl mb-8 font-semibold border-b pb-4">
@@ -39,7 +61,20 @@ const CommentsForm = ({ slug }) => {
                 
             </div>
             <div className="grid grid-cols-1 gap-4 mb-4">
-                
+                <div>
+                    <input ref={storeDataEl} type="checkbox" id="storeData" name="storeData" value="true"></input>
+                    <label className="text-gray-500 cursor-pointer ml-2" htmlFor="storeData">Salve meu e-mail e nome para a próxima vez que eu comentar.</label>
+                </div>
+            </div>
+            { error && <p className="text-xs text-red-500">Ops! Todos os campos são obrigatórios. 😅</p>}
+            <div className="mt-8">
+                <button 
+                    className="button" 
+                    onClick={handleCommentSubmission}
+                    className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3">
+                Postar Comentário
+                </button>
+                {showSuccessMessage && <span className="text-xl float-right font-semibold mt-3 text-green-500">Comentário foi submetido para avaliação. 😉👍</span>}
             </div>
         </div>
     )
